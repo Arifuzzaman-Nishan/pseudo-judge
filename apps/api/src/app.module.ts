78df8165de33f9e13eeac26742d042b8a82b69ab
Arifuzzaman-Nishan <arifuzzamannishan@gmail.com>
2024-01-11 00:49:10 +0600

    some changes are made

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,13 +6,17 @@ import { AllExceptionsFilter } from './shared/filters/all-exceptions.filter';
 import { GroupModule } from './group/group.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import path from 'path';
 
 const APP_FILTER = 'APP_FILTER';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `env/.env.${process.env.NODE_ENV}`,
+      envFilePath: path.resolve(
+        __dirname,
+        `../../../env/.env.${process.env.NODE_ENV}`,
+      ),
     }),
     DatabaseModule,
     AuthModule,
--- apps/web/src/lib/tanstackQuery/api/baseApi.ts
+++ apps/web/src/lib/tanstackQuery/api/baseApi.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+const apiUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://pseudojudge.codes/api/v1"
+    : "http://localhost:5000/api/v1";
 
 const baseApi = axios.create({
   baseURL: apiUrl,
